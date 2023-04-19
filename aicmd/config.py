bc12052314a4495138ec8bc3eb56be2efab8b82e
Krystian Kaczyński <krystian@toskk.pl>
2023-04-19 19:22:50 +0200

    Early stage project structure and basic configuration

---
+++ aicmd/config.py
@@ -0,0 +1,10 @@
+import json
+import os
+
+def load_settings():
+    settings_file_path = os.path.join(os.path.dirname(__file__), 'settings.json')
+    with open(settings_file_path) as settings_file:
+        settings = json.load(settings_file)
+    return settings
+
+settings = load_settings()
